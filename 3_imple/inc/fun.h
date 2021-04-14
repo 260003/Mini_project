9f98a9099edf0e419bf2cf50201979381724517f
260003 <80737226+260003@users.noreply.github.com>
2021-04-14 16:38:28 +0530

    Add files via upload

---
+++ 3_imple/inc/fun.h
@@ -0,0 +1,28 @@
+/**
+ * @file fun.h
+ * @author Rahul 
+ * @brief Header file for bank manegment System
+ * @version 0.1
+ * @date 2021-04-14
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef __FUN_H__
+#define __FUN_H__
+
+
+void Create_new_account();
+void Cash_Deposit();
+void Cash_withdrawl();
+void Account_information();
+void Log_out();
+void display_options();
+/**
+ * @brief various functions
+
+ */
+
+
+#endif  /* __FUN_H__  */
