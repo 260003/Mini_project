fd0989f813dfcb8de3ad46ad92c300d0b7dc85c3
260003 <80737226+260003@users.noreply.github.com>
2021-04-14 16:23:11 +0530

    Add files via upload

---
+++ 3_imple/inc/fun.h
@@ -0,0 +1,36 @@
+/**
+ * @file fun.h
+ * @author Bharath G
+ * @brief Header file for sum and square functions
+ * @version 0.1
+ * @date 2021-03-16
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef __FUN_H__
+#define __FUN_H__
+/**
+ * @brief Funtion to find the sum of two integer numbers
+ *
+ * @param[in] number1
+ * @param[in] number2
+ * @return int result of the sum operation
+ */
+
+void Create_new_account();
+void Cash_Deposit();
+void Cash_withdrawl();
+void Account_information();
+void Log_out();
+void display_options();
+/**
+ * @brief Function to find square of a integer number
+ *
+ * @param[in] number Number for which the square has to be found
+ * @return int Result of the square operation
+ */
+
+
+#endif  /* __FUN_H__  */
