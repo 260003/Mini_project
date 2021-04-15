bb595a9645e6c2e686b8c6cf7eca58921e13ec64
260003 <80737226+260003@users.noreply.github.com>
2021-04-15 15:19:35 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,3 +1,14 @@
+/**
+ * @file main.c
+ * @author Rahul 
+ * @brief main file for bank manegment System
+ * @version 0.1
+ * @date 2021-04-14
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #include <stdio.h>
 //#include <conio.h>
 #include <string.h>
